3b5c1794e67c9281df4d24f545544ee7915a5bc7
moshiur01 <mr.moshiur314@gmail.com>
2023-10-03 00:34:18 +0600

    [DONE] add-a-new-book

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -61,18 +61,6 @@ export default function Navbar() {
 
               {isLoggedIn && (
                 <>
-                  <Link to="/wishlist">
-                    <li
-                      className={`${
-                        location.pathname === "/wishlist"
-                          ? "bg-sky-800 text-white"
-                          : "bg-slate-100"
-                      } text-sm rounded-md mx-2 py-1.5 font-medium w-28 text-center`}
-                    >
-                      Wishlist
-                    </li>
-                  </Link>
-
                   <Link to="/add-new-book">
                     <li
                       className={`${
@@ -94,7 +82,7 @@ export default function Navbar() {
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                  <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80" />
                 </div>
               </label>
               <ul
@@ -102,60 +90,29 @@ export default function Navbar() {
                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
+                  <Link
+                    to="/wishlist"
+                    className="justify-between py-2 mb-2 font-medium"
+                  >
+                    Wishlist
+                  </Link>
                 </li>
                 <li>
-                  <a>Settings</a>
+                  <Link
+                    to="/currently-reading"
+                    className="justify-between py-2 mb-2 font-medium"
+                  >
+                    Currently Reading
+                  </Link>
                 </li>
                 <li>
-                  <a>Logout</a>
+                  <span
+                    onClick={handleLogout}
+                    className="py-2 mb-1 font-medium hover:bg-red-500 hover:text-white "
+                  >
+                    Logout
+                  </span>
                 </li>
-              </ul>
-            </div>
-
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                  <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80" />
-                </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="mt-3 z-[1] p-2 shadow-md border menu menu-sm dropdown-content bg-slate-100 rounded-box w-52"
-              >
-                {isLoggedIn ? (
-                  <>
-                    <li>
-                      <Link
-                        to="/currently-reading"
-                        className="justify-between py-2 mb-2 font-medium"
-                      >
-                        Currently Reading
-                      </Link>
-                    </li>
-
-                    <li>
-                      <span
-                        onClick={handleLogout}
-                        className="py-2 mb-1 font-medium hover:bg-red-500 hover:text-white "
-                      >
-                        Logout
-                      </span>
-                    </li>
-                  </>
-                ) : (
-                  <li>
-                    <Link
-                      to="/login"
-                      className="py-2 mb-2 font-medium hover:bg-violet-600 hover:text-white"
-                    >
-                      Login
-                    </Link>
-                  </li>
-                )}
               </ul>
             </div>
           </div>
--- src/page/AddBook.tsx
+++ src/page/AddBook.tsx
@@ -61,140 +61,148 @@ export default function AddBook() {
 
   return (
     <div className="container mx-auto px-5">
-      <h2 className="text-center text-2xl font-semibold pt-10">Add Book</h2>
+      <h2 className="text-center text-3xl font-bold py-8 text-sky-800">
+        Add a New Book
+      </h2>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="max-w-sm mx-auto mt-10 pb-20">
-          <div>
-            <h3 className="poppins text-base font-medium mb-2 ">Title</h3>
-            <input
-              placeholder="Type book title"
-              type="text"
-              className={`border w-full outline-none  py-2 px-3 ${
-                errors.title
-                  ? " border-red-500 focus:border-red-500"
-                  : "focus:border-slate-700 border-slate-300"
-              }`}
-              {...register("title", {
-                required: {
-                  value: true,
-                  message: "Title is required",
-                },
-              })}
-            />
-            {errors.title && (
-              <span className="label-text-alt text-red-500 text-sm">
-                {errors.title.message}
-              </span>
-            )}
-          </div>
-          <div>
-            <h3 className="poppins text-base font-medium mb-2 mt-2 ">Author</h3>
-            <div className="">
-              <input
-                placeholder="Type author name"
-                type="text"
-                className={`border w-full outline-none py-2 px-3 ${
-                  errors.author
-                    ? " border-red-500 focus:border-red-500"
-                    : "focus:border-slate-700 border-slate-300"
-                }`}
-                {...register("author", {
-                  required: {
-                    value: true,
-                    message: "Author is required",
-                  },
-                })}
-              />
-            </div>
-            {errors.author && (
-              <span className="label-text-alt text-red-500 text-sm">
-                {errors.author.message}
-              </span>
-            )}
-          </div>
-          <div>
-            <h3 className="poppins text-base font-medium mb-2 mt-2 ">Genre</h3>
-            <div className="">
-              <input
-                type="text"
-                placeholder="Select genre name"
-                className={`border w-full outline-none py-2 px-3 ${
-                  errors.genre
-                    ? " border-red-500 focus:border-red-500"
-                    : "focus:border-slate-700 border-slate-300"
-                }`}
-                {...register("genre", {
-                  required: {
-                    value: true,
-                    message: "Genre is required",
-                  },
-                })}
-              />
-            </div>
-            {errors.genre && (
-              <span className="label-text-alt text-red-500 text-sm">
-                {errors.genre.message}
-              </span>
-            )}
-          </div>
-          <div>
-            <h3 className="poppins text-base font-medium mb-2 mt-2 ">
-              Publication Year
-            </h3>
-            <div className="">
-              <input
-                type="date"
-                placeholder="Select publication year"
-                className={`border w-full outline-none py-2 px-3 ${
-                  errors.publication
-                    ? " border-red-500 focus:border-red-500"
-                    : "focus:border-slate-700 border-slate-300"
-                }`}
-                {...register("publication", {
-                  required: {
-                    value: true,
-                    message: "Publication is required",
-                  },
-                })}
-              />
-            </div>
-            {errors.publication && (
-              <span className="label-text-alt text-red-500 text-sm">
-                {errors.publication.message}
-              </span>
-            )}
-          </div>
-          <div>
-            <h3 className="poppins text-base font-medium mb-2 mt-2 ">Image</h3>
-            <div className="">
-              <input
-                placeholder="Image link ( Optional )"
-                type="text"
-                className={`border w-full outline-none py-2 px-3 focus:border-slate-700 border-slate-300`}
-                {...register("image")}
-              />
-            </div>
-          </div>
-          <div>
-            <h3 className="poppins text-base font-medium mb-2 mt-2 ">
-              Summary
-            </h3>
-            <div className="">
-              <textarea
-                placeholder="Summary ( Optional )"
-                type="text"
-                className={`border w-full outline-none py-2 px-3 focus:border-slate-700 border-slate-300`}
-                {...register("summary")}
-              />
-            </div>
-          </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="max-w-lg mx-auto p-6 bg-white rounded-lg shadow-lg"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="title"
+            className="block text-lg font-medium text-gray-800"
+          >
+            Title
+          </label>
           <input
-            // disabled={isLoading}
-            className="bg-violet-600 text-white mt-10 w-full py-2 text-lg poppins font-semibold cursor-pointer uppercase"
-            type="submit"
-            value="Submit"
+            id="title"
+            placeholder="Enter the book title"
+            type="text"
+            className={`border border-gray-300 rounded-md w-full py-2 px-3 focus:ring focus:ring-blue-500`}
+            {...register("title", {
+              required: {
+                value: true,
+                message: "Title is required",
+              },
+            })}
           />
+          {errors.title && (
+            <span className="text-red-500 text-sm">{errors.title.message}</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="author"
+            className="block text-lg font-medium text-gray-800"
+          >
+            Author
+          </label>
+          <input
+            id="author"
+            placeholder="Enter the author's name"
+            type="text"
+            className={`border border-gray-300 rounded-md w-full py-2 px-3 focus:ring focus:ring-blue-500`}
+            {...register("author", {
+              required: {
+                value: true,
+                message: "Author is required",
+              },
+            })}
+          />
+          {errors.author && (
+            <span className="text-red-500 text-sm">
+              {errors.author.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="genre"
+            className="block text-lg font-medium text-gray-800"
+          >
+            Genre
+          </label>
+          <input
+            id="genre"
+            type="text"
+            placeholder="Enter the genre"
+            className={`border border-gray-300 rounded-md w-full py-2 px-3 focus:ring focus:ring-blue-500`}
+            {...register("genre", {
+              required: {
+                value: true,
+                message: "Genre is required",
+              },
+            })}
+          />
+          {errors.genre && (
+            <span className="text-red-500 text-sm">{errors.genre.message}</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="publication"
+            className="block text-lg font-medium text-gray-800"
+          >
+            Publication Year
+          </label>
+          <input
+            id="publication"
+            type="date"
+            placeholder="Select the publication year"
+            className={`border border-gray-300 rounded-md w-full py-2 px-3 focus:ring focus:ring-blue-500`}
+            {...register("publication", {
+              required: {
+                value: true,
+                message: "Publication year is required",
+              },
+            })}
+          />
+          {errors.publication && (
+            <span className="text-red-500 text-sm">
+              {errors.publication.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="image"
+            className="block text-lg font-medium text-gray-800"
+          >
+            Image
+          </label>
+          <input
+            id="image"
+            placeholder="Enter the image link (Optional)"
+            type="text"
+            className={`border border-gray-300 rounded-md w-full py-2 px-3 focus:ring focus:ring-blue-500`}
+            {...register("image")}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="summary"
+            className="block text-lg font-medium text-gray-800"
+          >
+            Summary
+          </label>
+          <textarea
+            id="summary"
+            placeholder="Enter a book summary (Optional)"
+            rows="4"
+            className={`border border-gray-300 rounded-md w-full py-2 px-3 focus:ring focus:ring-blue-500`}
+            {...register("summary")}
+          />
+        </div>
+
+        <div className="mx-44">
+          <button
+            className="bg-sky-800 text-white py-3 px-6 rounded-md font-semibold hover:bg-sky-700 focus:outline-none focus:ring focus:ring-sky-800 "
+            type="submit"
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>

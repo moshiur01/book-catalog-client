46704350eaffac042678f7d6c519a75f3608e627
moshiur01 <mr.moshiur314@gmail.com>
2023-10-03 11:47:17 +0600

    [DONE] navbar user data handled

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -9,7 +9,7 @@ export default function ReviewCard({ review }: { review: any }) {
         <div>
           <div className="avatar">
             <div className="w-16 rounded-full shadow-lg shadow-gray-400 ring-offset-2">
-              <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80" />
+              <img src="https://cdn-icons-png.flaticon.com/512/666/666201.png" />
             </div>
           </div>
         </div>
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -77,120 +77,59 @@ export default function Navbar() {
             </ul>
           </div>
           <div className="hidden lg:flex gap-2">
-            {isLoggedIn && <span className="font-medium">{user!.name}</span>}
-
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full">
-                  <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80" />
+                  <img src="https://cdn-icons-png.flaticon.com/512/666/666201.png" />
                 </div>
               </label>
               <ul
                 tabIndex={0}
                 className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
-                <li>
-                  <Link
-                    to="/wishlist"
-                    className="justify-between py-2 mb-2 font-medium"
-                  >
-                    Wishlist
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/currently-reading"
-                    className="justify-between py-2 mb-2 font-medium"
-                  >
-                    Currently Reading
-                  </Link>
-                </li>
-                <li>
-                  <span
-                    onClick={handleLogout}
-                    className="py-2 mb-1 font-medium hover:bg-red-500 hover:text-white "
-                  >
-                    Logout
-                  </span>
-                </li>
+                {isLoggedIn && (
+                  <>
+                    <li>
+                      <Link
+                        to="/wishlist"
+                        className="justify-between py-2 mb-2 font-medium"
+                      >
+                        Wishlist
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/currently-reading"
+                        className="justify-between py-2 mb-2 font-medium"
+                      >
+                        Currently Reading
+                      </Link>
+                    </li>
+                  </>
+                )}
+
+                {isLoggedIn ? (
+                  <li>
+                    <span
+                      onClick={handleLogout}
+                      className="py-2 mb-1 font-medium hover:bg-red-500 hover:text-white "
+                    >
+                      Logout
+                    </span>
+                  </li>
+                ) : (
+                  <li>
+                    <Link to="/login">
+                      <li className="justify-between  mb-2 font-medium">
+                        Login
+                      </li>
+                    </Link>
+                  </li>
+                )}
               </ul>
             </div>
           </div>
         </div>
-        {/* Page content here */}
-        {/* Content here */}
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="navbar-drawer" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-64 h-full bg-black text-white">
-          {/* Sidebar content here */}
-
-          <p className="text-center text-lg lg:text-xl mb-5">Book Catalog</p>
-          <Link to="/books">
-            <li
-              className={`${
-                location.pathname === "/books" ? "bg-slate-500" : "bg-slate-900"
-              }  rounded-md py-1.5 px-3 mb-2`}
-            >
-              All Books
-            </li>
-          </Link>
-
-          {isLoggedIn && (
-            <>
-              <Link to="/wishlist">
-                <li
-                  className={`${
-                    location.pathname === "/wishlist"
-                      ? "bg-slate-500"
-                      : "bg-slate-900"
-                  }  rounded-md py-1.5 px-3 mb-2`}
-                >
-                  Wishlist
-                </li>
-              </Link>
-              <Link to="/currently-reading">
-                <li
-                  className={`${
-                    location.pathname === "/currently-reading"
-                      ? "bg-slate-500"
-                      : "bg-slate-900"
-                  }  rounded-md py-1.5 px-3 mb-2`}
-                >
-                  Currently Reading
-                </li>
-              </Link>
-
-              <Link to="/add-new-book">
-                <li
-                  className={`${
-                    location.pathname === "/add-new-book"
-                      ? "bg-slate-500"
-                      : "bg-slate-900"
-                  }  rounded-md py-1.5 px-3 mb-2`}
-                >
-                  Add Book
-                </li>
-              </Link>
-            </>
-          )}
-
-          <div className="my-10"></div>
-          {isLoggedIn ? (
-            <li
-              onClick={handleLogout}
-              className="bg-red-600 rounded-md py-1.5 px-3 mb-2"
-            >
-              Logout
-            </li>
-          ) : (
-            <Link to="/login">
-              <li className="bg-violet-600 rounded-md py-1.5 px-3 mb-2">
-                Login
-              </li>
-            </Link>
-          )}
-        </ul>
       </div>
     </div>
   );
